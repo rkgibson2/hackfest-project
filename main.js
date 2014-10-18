32f40d53de5e2a5cc90854e74ce342c0b04fb3f7
huihuifan <anarfish@gmail.com>
2014-10-18 15:10:23 -0400

    added katarina image to header

--- main.js
+++ main.js
@@ -1,2 +1,7 @@
 //Angela Fan, Robbie Gibson
 
+//corner hero image
+
+var elem = document.createElement("img")
+document.getElementById("hero_image").appendChild(elem);
+elem.src = "/img/champs/katarina.png";
